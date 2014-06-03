9377ed1af361ae50e15f6e1314dd405ff21f1944
JeanCarloMachado <j34nc4rl0@gmail.com>
2014-06-03 00:19:56 -0300

    itautig with full name for git commit instead of the alias

--- aliases.sh
+++ aliases.sh
@@ -2,6 +2,14 @@
 
 currDir="`pwd`/"
 
+[ ! -z $1 ] && [ -d $1 ] && {
+
+currDir=$1
+
+echo "using user passed directory <$currDir>"
+
+}
+
 alias gc=$currDir"git-commit"
 alias gsc=$currDir"git-submodules-commit"
 alias gsf=$currDir"git-submodules-foreach"
