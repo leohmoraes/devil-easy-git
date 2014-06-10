bb611c84b516adce025898b42b8cb655104d5ad0
Jean Carlo Machado <j34nc4rl0@gmail.com>
2014-06-10 01:01:47 -0300

    vagrant env ready to go

--- aliases.sh
+++ aliases.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-currDir="`pwd`/"
 
-[ ! -z $1 ] && [ -d $1 ] && {
-
-currDir=$1
-
-echo "using user passed directory <$currDir>"
-
-}
-
-alias gc=$currDir"git-commit"
-alias gsc=$currDir"git-submodules-commit"
-alias gsf=$currDir"git-submodules-foreach"
-alias gsa=$currDir"git-submodules-add"
+alias gc="git-commit"
+alias gsc="git-submodules-commit"
+alias gsf="git-submodules-foreach"
+alias gsa="git-submodules-add"
 
 
